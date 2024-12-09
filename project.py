00950bef9c98b093c970962ae05fcfb08375ebe7
Tobi <stdio11games@gmail.com>
2024-12-09 15:51:56 +0100

    Finsihed

--- project.py
+++ project.py
@@ -1,137 +1,138 @@
 import sys
-import tkinter as tk
-from re import match as regex_match
-from tkinter import messagebox
+import sqlite3
+import customtkinter as ctk
+import pytweening as tweener
 from datetime import datetime as time
 
 
+ctk.set_appearance_mode("dark")
 
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("500x300")
+        self.custom_font =("Lato",20,'normal')
+        self.button_height = 60
+        self.button_width = 180
+        self.title("Pe-FI")
+        self.account = Account()
+
+        # Create buttons
+        self.button_frame = ctk.CTkFrame(self,corner_radius= 12,width= 250,height=300,border_width=0.5,fg_color="#1E1E1E")
+        self.balance_button = ctk.CTkButton(self.button_frame, text="Balance",corner_radius= 6,font=self.custom_font,height=self.button_height,width=self.button_width,command = self.balance_command)
+        self.withdraw_button = ctk.CTkButton(self.button_frame, text="Withdraw",corner_radius= 6,font=self.custom_font,height=self.button_height,width=self.button_width,command = self.widthdraw_command)
+        self.deposit_button = ctk.CTkButton(self.button_frame, text="Deposit",corner_radius=6,font= self.custom_font,height=self.button_height,width=self.button_width,command=self.deposit_command)
+        self.greeting = ctk.CTkLabel(self,text="Welcome to Pe-FI",font = ("Ariel",32,'bold'),fg_color="transparent", bg_color="transparent")
+        
+        # Place buttons using pack
+        self.balance_button.pack(pady = 10,anchor ="center")
+        self.withdraw_button.pack(pady = 7,anchor = "center")
+        self.deposit_button.pack(pady = 10,anchor = "center")
+        self.button_frame.place(relx=0, rely=0.18, relwidth=0.5, relheight=1)
+        self.greeting.grid(row=0, column=0, columnspan=3, pady=5, sticky="NSEW")
+
+    def balance_command(self):
+        self.input_frame = ctk.CTkFrame(self,corner_radius= 12,width= 250,height=300,border_width=0.45,fg_color="#1E1E1E")
+        self.input_frame.place(relx= 0.51, rely=0.18, relwidth=0.5, relheight=1)
+        balance_text = ctk.CTkLabel(self.input_frame,text="Your Balance is",font = ("Ariel",24,'bold'),anchor= "center")
+        balance_num = ctk.CTkLabel(self.input_frame,text="$"+str(self.account.balance),font = ("Ariel",38,'bold'),anchor= "center")
+        recent_transactions_label = ctk.CTkLabel(self.input_frame,text="Recent Transactions",font = ("Ariel",18,'bold'),fg_color="transparent", bg_color="transparent")
+        output = ""
+        for entries in self.account.recent_transactions(5):
+            transaction_type  ={"deposit":"Deposited","widthdraw":"Widthdrew"}
+            output += f"{transaction_type.get(entries[0])} {entries[1]}\n"
+        transactions = ctk.CTkLabel(self.input_frame,text= output,font = ("Ariel",10,'bold'),fg_color="transparent", bg_color="transparent")
+        balance_text.pack(pady = 3)
+        balance_num.pack(pady=5)
+        recent_transactions_label.pack(pady=5)
+        transactions.pack(pady = 5)
+                
+    
+    def deposit_command(self):
+        self.deposit_frame = ctk.CTkFrame(self,corner_radius= 12,width= 250,height=300,border_width=0.45,fg_color="#1E1E1E")
+        self.deposit_frame.place(relx= 0.51, rely=0.18, relwidth=0.5, relheight=1)
+        deposit_text = ctk.CTkLabel(self.deposit_frame,text="Deposit How Much",font = ("Ariel",14,'bold'),anchor= "w",padx = 1)
+        deposit_textbox = ctk.CTkEntry(self.deposit_frame)
+        enter_button = ctk.CTkButton(self.deposit_frame,text = "enter",command = lambda:deposit(deposit_textbox.get()))
+        deposit_text.pack(pady=5,)
+        deposit_textbox.pack(pady=5)
+        enter_button.pack(pady =5)
+        def deposit(amount):
+            deposit_textbox.delete(0, 'end')
+            status = self.account.deposit(amount)
+            status_text = ctk.CTkLabel(self.deposit_frame,text= status,font = ("Ariel",14,'bold'),anchor= "w",padx = 1)
+            status_text.pack(pady =5)
+            self.after(1000,lambda:status_text.destroy())
+
+    def widthdraw_command(self):
+        self.widthdraw_frame = ctk.CTkFrame(self,corner_radius= 12,width= 250,height=300,border_width=0.45,fg_color="#1E1E1E")
+        self.widthdraw_frame.place(relx= 0.51, rely=0.18, relwidth=0.5, relheight=1)
+        widthdraw_text = ctk.CTkLabel(self.widthdraw_frame,text="Widthdraw how much?",font = ("Ariel",14,'bold'),anchor= "center")
+        widthdraw_textbox = ctk.CTkEntry(self.widthdraw_frame,)
+        enter_button = ctk.CTkButton(self.widthdraw_frame,text = "enter",command = lambda:widthdraw(widthdraw_textbox.get()))
+        widthdraw_text.pack(pady =5)
+        widthdraw_textbox.pack(pady =5)
+        enter_button.pack(pady =5)
+        def widthdraw(amount):
+            widthdraw_textbox.delete(0, 'end')
+            status = self.account.widthdraw(amount)
+            status_text = ctk.CTkLabel(self.widthdraw_frame,text= status,font = ("Ariel",14,'bold'),anchor= "w",padx = 1)
+            status_text.pack(pady =5)
+            self.after(1000,lambda:status_text.destroy())
 
 class Account:
-    def log_it(func):
-        def wrapper(*args,**kwargs):
-            func(*args,**kwargs)
-            with open("transaction_history.txt","a") as file:
-                file.write(f"{str(func.__qualname__).split(".")[1].title()} at {str(time.now())}\n")
-        return wrapper
-    
     def __init__(self) -> None:
-        try:
-            with open("old_bank_statement.txt","r") as file:
-                self._balance = int(file.read())
-        except:
-            with open("old_bank_statement.txt","w") as file:
-                self._balance = 0
-                file.write("0")
-
+        self.database = sqlite3.connect("/home/tinu/Workspace/Python/Pe-FI/database.db")
+        self.cursor = self.database.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS account (id INTEGER PRIMARY KEY AUTOINCREMENT,transaction_type TEXT NOT NULL,amount INT NOT NULL,balance INT NOT NULL);")
+        self.cursor.execute("SELECT balance FROM account ORDER BY id DESC LIMIT 1;")
+        result = self.cursor.fetchone()
+        if result == None:
+            self.cursor.execute( "INSERT INTO account (transaction_type, amount, balance) VALUES ('deposit', 0, 0);")
+            self.database.commit()
+            self.cursor.execute("SELECT balance FROM account ORDER BY id DESC LIMIT 1;")
+            self.balance = self.cursor.fetchone()[0]
+        else:
+            self.balance = result[0]
+           
     @property
     def balance(self):
         return self._balance
-
 
     @balance.setter
     def balance(self,amount):
         if amount >= 0:
             self._balance = amount
     
-    @log_it
     def deposit(self,amount):
         if amount.isdigit():
-            self._balance += int(amount) 
-            Popup(f"Done! Balance is {self.balance}","Balance")
+            self.balance += int(amount)
+            self.cursor.execute( "INSERT INTO account (transaction_type, amount, balance) VALUES ('deposit', ?, ?);",(amount,self.balance))
+            self.database.commit()
+            return "Sucessful"
         else:
-            Popup("Invalid Amount")
+            return "Invalid Input"
 
-    @log_it
     def widthdraw(self,amount):
         if amount.isdigit():
             if int(amount) <= self._balance:
-                self._balance -= int(amount)
-                Popup(f"Done! Balance is {self.balance}","Balance")
+                self.balance -= int(amount)
+                self.cursor.execute( "INSERT INTO account (transaction_type, amount, balance) VALUES ('widthdraw', ?, ?);",(amount,self.balance))
+                self.database.commit()
+                return "Sucessful"
             else:
-                Popup("Insufficent Funds")
+                return "Insufficent Funds"
         else:
-            Popup("Invalid Amount")
+            return "Invalid Input"
 
-
-class Window:
-    def __init__(self,name,size) -> None:
-        self._name = name
-        self._size = size
-        self._window = tk.Tk()
-        self._window.title(self._name)
-        self._window.geometry(self._size)
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self,name):
-        if name.isalpha():
-            self._name = name
-
-    @property
-    def size(self):
-        return self._size
-        
-    @size.setter
-    def size(self,size):
-        if regex_match(r"^\d{1,3}-\d{1,3}x\d{1,3}-\d{1,3}$",size):
-            self._size = size
-
-    def add_label(self,text:str,font:tuple):
-        tk.Label(self._window,text = text ,font = font,).pack()
-
-    def add_button(self,text:str,font:tuple,command):
-        button = tk.Button(self._window,text = text,font = font, command= command)
-        button.pack(pady= 20)
-
-    def add_textbox(self):
-         text_box = tk.Entry(self._window)
-         text_box.pack()
-         return text_box
-
-    def __str__(self) -> str:
-        return str(self.balance)
-    
-    def mainloop(self):
-        self._window.mainloop()
-
-class Popup:
-    def __init__(self,text,title = "window") -> None:
-        messagebox.showinfo(title,str(text))
-
-account = Account()
+    def recent_transactions(self,no_of_transactions):
+        self.cursor.execute("SELECT transaction_type,amount FROM account ORDER BY id DESC LIMIT ?;",(no_of_transactions,))
+        return self.cursor.fetchall()
 
 def main():   
-    window = Window("pefi","240x480")
-    window.add_label("Welcome to Pefi",('Roboto',18))
-    window.add_button("Check Balance",('Roboto',18),lambda:Popup(f"Balance is {account.balance}"))
-    window.add_button("Deposit",('Roboto',18),deposit_window)
-    window.add_button("Widthdraw",('Roboto',18),widthdraw_window)
-    window.add_button("Close",('Roboto',18),close_window)
-    window.mainloop()
-
-def widthdraw_window():
-    widthdraw_window = Window("Widthdraw","240x480")
-    widthdraw_window.add_label("Widthdraw how much",('Roboto',8))
-    text_box = widthdraw_window.add_textbox()
-    widthdraw_window.add_button("Enter",('Roboto',18),lambda:account.widthdraw(text_box.get()))
-    widthdraw_window.mainloop()
-
-
-def deposit_window():
-    deposit_window = Window("Deposit","240x480")
-    deposit_window.add_label("Deposit how much",('Roboto',8))
-    text_box = deposit_window.add_textbox()
-    deposit_window.add_button("Enter",('Roboto',18),lambda:account.deposit(text_box.get()))
-    deposit_window.mainloop()
-
-def close_window():
-    with open("old_bank_statement.txt","w") as file:
-        file.write(str(account.balance))
-    sys.exit(0)
+    app = App()
+    app.grid_columnconfigure(1, weight=1)
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
